7526540be2a2a49b60646798025fbd5346b0d804
Alka Sah <alkasah485@gmail.com>
2024-07-18 10:13:40 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 //import { useState } from 'react'
 import React from 'react'
-
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+//--------------------------------------------------import dependencies
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Workout from './components/Workout'
-
+//----------------------------------------------import components
 import './App.css'
 
 function App() {
-  
 
   return (
     <>
-      <Home/>
+    <Router>
+      <div className='app'>
       <Navbar/>
-      <Workout/>
+      <Routes>
+        <Route exactpath ="/" Component={<Home/>}></Route>
+        <Route exactpath = "/workout" Component={<Workout/>}></Route>
+
+      </Routes>
+
+      </div>
+
+    </Router>
     </>
   )
 }
