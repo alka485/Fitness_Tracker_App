e92e28ce4b8250cc0457dba344a3b769a19346c9
Alka Sah <alkasah485@gmail.com>
2024-07-18 10:40:39 -0700

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 import React from 'react'
-import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 //--------------------------------------------------import dependencies
 import Home from './components/Home'
 import Navbar from './components/Navbar'
@@ -12,18 +12,14 @@ function App() {
 
   return (
     <>
-    <Router>
       <div className='app'>
-      <Navbar/>
+      <Navbar>
       <Routes>
-        <Route exactpath ="/" Component={<Home/>}></Route>
-        <Route exactpath = "/workout" Component={<Workout/>}></Route>
-
+        <Route exactpath ="/"  element={<Home/>}></Route>
+        <Route exactpath = "/workout"  element={<Workout/>}></Route>
       </Routes>
-
+      </Navbar>
       </div>
-
-    </Router>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+        <Link to='/'>Home</Link>
+        <Link to="/workout">Workout</Link>
+
+    </nav>
   )
 }
 
